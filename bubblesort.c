9b2210f8a86143938b65902e081c5cf8e7b48c0b
PhaniLakshmiKanth <lakshmikanthkolipaka@gmail.com>
2021-11-24 17:47:55 +0530

    added print

--- bubblesort.c
+++ bubblesort.c
@@ -33,6 +33,7 @@ int main()
 {
 	int arr[] = {64, 34, 25, 12, 22, 11, 90};
 	int n = sizeof(arr)/sizeof(arr[0]);
+	printf("My first program\n");
 	bubbleSort(arr, n);
 	printf("Sorted array: \n");
 	printArray(arr, n);
